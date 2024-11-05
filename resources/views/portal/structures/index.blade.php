42ee2961d27de59bd3a05131c072902771db0840
Hoàng Trọng Bình <binhkc1999@gmail.com>
2024-11-05 15:00:42 +0900

    ud

--- resources/views/portal/structures/index.blade.php
+++ resources/views/portal/structures/index.blade.php
@@ -11,7 +11,6 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 <p>Bộ phận</p>
-                <!-- <button id="btn-add-level" type="button" class="btn btn-primary">Add Level</button> -->
             </div>
             <div class="panel-body" style="height: 70vh; overflow-y: auto;">
                 <div class="dataTable_wrapper">
@@ -68,28 +67,33 @@
     </div>
     <input style="display: none" type="none" value="{{ Request::get('level') }}" id="level_id">
 </div>
+<div class="modal fade" id="ReactModalPortal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background: #eaeaea;">
+        <h4 class="modal-title" id="exampleModalLongTitle"
+          style="width: 100%;
+          text-align: center;
+          font-weight: 700;
+          font-style: normal;
+          font-size: 15px;
+          line-height: 24px;
+          color: #213671;
+          text-transform: uppercase;">
+          Modal title
+        </h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" style="height: 65vh; overflow-y: auto; overflow-x: hidden;">
+        <div class="row row-eq-height">
 
-<!-- <div class="modal fade" id="addLevelModal" tabindex="-1" role="dialog" aria-labelledby="addLevelModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="addLevelModalLabel">Thêm mới phòng ban</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form id="addLevelForm">
-                    <div class="form-group">
-                        <label for="levelTitle">Tên phòng ban</label>
-                        <input type="text" class="form-control" id="levelTitle" name="title" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Thêm mới</button>
-                </form>
-            </div>
         </div>
+      </div>
     </div>
-</div> -->
+  </div>
+</div>
 @endsection
 
 @section('js')
@@ -119,31 +123,6 @@
                 // });
             }
         });
-        // Show modal when "Add Level" button is clicked
-        // $("#btn-add-level").on("click", function() {
-        //     $("#addLevelModal").modal("show");
-        // });
-
-        // // Submit form via AJAX
-        // $("#addLevelForm").on("submit", function(e) {
-        //     e.preventDefault();
-        //     const title = $("#levelTitle").val();
-
-        //     $.ajax({
-        //         url: "{{ route('levels.create') }}",
-        //         type: "POST",
-        //         data: {
-        //             title: title,
-        //         },
-        //         success: function(response) {
-        //             location.reload();
-        //         },
-        //         error: function(error) {
-        //             console.error("Error adding level:", error);
-        //             alert("Failed to add level.");
-        //         }
-        //     });
-        // });
     });
     $(document).on("click", "#btn-update-structure", function() {
         const _this = $(this)
